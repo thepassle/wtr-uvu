bea00857b521915a5e6af0727cc2734c76b9aa88
thepassle <pascalschilp@gmail.com>
2021-09-26 14:06:34 +0200

    refactor: improve fork

--- test/uvu-fork.js
+++ test/uvu-fork.js
@@ -1,130 +1,134 @@
-globalThis.UVU_QUEUE = [[]];
+const timer = (now = Date.now()) => () => (Date.now() - now).toFixed(2) + 'ms';
 
-let hrtime = (now = Date.now()) => () => (Date.now() - now).toFixed(2) + 'ms';
-const into = (ctx, key) => (name, handler) => ctx[key].push({ name, handler });
-const hook = (ctx, key) => handler => ctx[key].push(handler);
-const context = (state) => ({ tests:[], before:[], after:[], bEach:[], aEach:[], only:[], skips:0, state });
-
-async function startTestRunner(ctx, name, renderer) {
-	let { only, tests, before, after, bEach, aEach, state } = ctx;
-	let hook, test, arr = only.length ? only : tests;
-  const resultTemplate = {
-    name: '',
-    passed: true,
-    skipped: false,
-    duration: '',
-    error: false
-  }
+async function run(suite, {renderer}) {
+  const { name, only, tests, before, after, beforeEach, afterEach } = suite;
+  const testsToRun = only.length ? only : tests;
 
   const testSuiteResult = {
-    name: state.__suite__,
-    suites: [],
+    name,
     tests: []
   }
 
-	try {
-		for (hook of before) await hook(state);
+  try {
+    await before?.();
 
-		for (test of arr) {
-      const result = {...resultTemplate};
-      result.name = test.name;
-      const time = hrtime();
-			try {
-				for (hook of bEach) await hook(state);
-        
-				const { skipped } = (await test.handler(state)) || {};
-        result.duration = time();
-        result.skipped = !!skipped;
-
-				for (hook of aEach) await hook(state);
-			} catch (err) {
-        result.passed = false;
-        for (hook of aEach) await hook(state);
-        
-        result.error = {
-          stack: err.stack,
-          actual: err.actual,
-          expects: err.expects,
-          operator: err.operator,
-          message: err.message,
-          details: err.details,
-          generated: err.generated,
-        }
-			} finally {
-        renderer?.(result);
-        testSuiteResult.tests.push(result);
+    for (const test of testsToRun) {
+      const testResult = {
+        name: test.name,
+        passed: true,
+        skipped: false,
+        duration: '',
+        error: false  
       }
-		}
-	} finally {
-		for (hook of after) await hook(state);
+
+      const time = timer();
+      try {
+        await beforeEach?.();
+        
+        const { skipped } = (await test.handler()) || {};
+        testResult.duration = time();
+        testResult.skipped = !!skipped;
+
+        await afterEach?.();
+      } catch (err) {
+        testResult.passed = false;
+        testResult.duration = time();
+
+        await afterEach?.();
+        
+        /** @TODO Make sure we can access error.stack, message, actual, etc correctly on this */
+        /** @TODO this will currently only work with uvu/assert, because the assert error has an `.expects` property */
+        testResult.error = {...err, expected: err.expects};
+      } finally {
+        renderer?.(testResult);
+        testSuiteResult.tests.push(testResult);
+      }
+    }
+  } finally {
+    await after?.();
 
     return testSuiteResult;
-	}
-}
-
-function setup(ctx, name = '') {
-	ctx.state.__test__ = '';
-	ctx.state.__suite__ = name;
-
-	const test = into(ctx, 'tests');
-  
-	test.before = hook(ctx, 'before');
-	test.before.each = hook(ctx, 'bEach');
-	test.after = hook(ctx, 'after');
-	test.after.each = hook(ctx, 'aEach');
-	test.only = into(ctx, 'only');
-	test.skip = (name) => { 
-    ctx.tests.push({ handler: () => ({skipped: true}), name});
-  };
-	test.queue = () => {
-		let copy = { ...ctx };
-		let run = startTestRunner.bind(0, copy, name);
-		Object.assign(ctx, context(copy.state));
-		UVU_QUEUE[0].push(run);
-	};
-	return test;
-}
-
-export const suite = (name = '', state = {}) => setup(context(state), name);
-export const test = suite();
-
-export async function exec({renderer}) {
-	let timer = hrtime();
-	let totalTests=0, skippedTests=0, failedTests=0, errors=[];
-  let hasErrored = false;
-
-  const groupResults = [];
-
-	for (let group of UVU_QUEUE) {
-
-		for (let startTestRunner of group) {
-
-      const testSuiteResult = await startTestRunner(renderer);
-      groupResults.push(testSuiteResult);
-
-      totalTests += testSuiteResult.tests.length;
-      skippedTests += testSuiteResult.tests.filter(test => test.skipped).length;
-      errors = testSuiteResult.tests.filter(test => test.error).map(test => test.error);
-      failedTests += testSuiteResult.tests.filter(test => test.error).length;
-      
-      if(!hasErrored) {
-        hasErrored = !testSuiteResult.tests.some(test => test.error);
-      }
-		}
-	}
-
-  return {
-    status: 'FINISHED',
-    totalTests,
-    skippedTests,
-    failedTests,
-    duration: timer(),
-    passed: hasErrored,
-    errors,
-    testResults: {
-      name: '',
-      suites: [],
-      tests: groupResults,
-    },
   }
 }
+
+const queue = [];
+
+export const createSuite = (name = '') => {
+  const suite = { name, tests:[], only:[] };
+  
+  const test = (name, handler) => {
+    suite.tests.push({name, handler});
+  }
+
+  test.before = handler => { suite.before = handler; };
+  test.beforeEach = handler => { suite.beforeEach = handler; };
+  test.after = handler => { suite.after = handler; };
+  test.afterEach = handler => { suite.afterEach = handler; };
+  test.only = handler => { suite.only.push(handler); };
+  test.skip = name => {
+    suite.tests.push({handler: () => ({skipped: true}), name});
+  }
+
+  queue.push(suite);
+
+  return test;
+}
+
+export const suite = createSuite;
+export const test = createSuite();
+
+export async function executeTests({ renderer }) {
+  const time = timer();
+  const suiteResults = [];
+  let total = 0;
+  let skipped = 0;
+  let failed = 0;
+
+  for(const suite of queue) {
+    const result = await run(suite, {renderer});
+    suiteResults.push(result);
+
+    for(const test of result.tests) {
+      total++;
+      if(test.skipped) skipped++;
+      if(test.error) failed++;
+    }
+  }
+
+  return { 
+    status: 'FINISHED',
+    total,
+    skipped,
+    failed,
+    passed: failed < 1,
+    duration: time(),
+    results: suiteResults,
+
+    /** @TODO map `results` to what WTR expects */
+    // testResults: {
+    //   name: '',
+    //   suites: suiteResults,
+    // }
+  }
+}
+
+
+const suite2 = suite('suite 2');
+suite2('hello foo', () => {
+  console.log('suite 2');
+});
+
+test('hello', () => {
+  console.log('test 1');
+});
+
+executeTests({
+  renderer: (testResult) => {
+    const report = document.createElement('test-report');
+    report.testResult = testResult;
+    document.body.appendChild(report);
+  }
+}).then(r => {
+  console.log(r);
+});
+
